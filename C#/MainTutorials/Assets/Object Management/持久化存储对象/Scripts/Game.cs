1471aca3ca7913542c33aac8896b80c4ac6864b2
429078038@qq.com <429078038@qq.com>
2019-08-05 22:15:41 +0800

    2019.8.5
    对象池及场景池的管理及维护

--- C#/MainTutorials/Assets/Object Management/持久化存储对象/Scripts/Game.cs
+++ C#/MainTutorials/Assets/Object Management/持久化存储对象/Scripts/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 //Game流程控制类
 public class Game : PersistableObject
@@ -9,6 +10,13 @@ public class Game : PersistableObject
     public ShapeFactory shapeFactory;
     [Header("序列化管理器")]
     public PersistentStorage storage;
+
+    public float CreationSpeed { get; set; }
+
+    public float DestructionSpeed { get; set; }
+
+
+
     [Header("创建")]
     public KeyCode creatKey = KeyCode.C;
     [Header("清除")]
@@ -17,39 +25,87 @@ public class Game : PersistableObject
     public KeyCode saveKey = KeyCode.S;
     [Header("加载")]
     public KeyCode loadKey = KeyCode.L;
+    [Header("销毁")]
+    public KeyCode destoryKey = KeyCode.X;
+
+    public int LevelCount;
 
     List<Shape> shapes;
-    
-    //版本标记
-    const int saveVersion = 1;
 
-    void Awake()
+    //版本标记
+    const int saveVersion = 2;
+
+    private float creationProgress, destructionProgress;
+
+    private int loadedLevelBuildIndex;
+
+    void Start()
     {
         shapes = new List<Shape>();
+
+        if (Application.isEditor)
+        {
+            for (int i = 0; i < SceneManager.sceneCount; i++)
+            {
+                Scene loadedLevlel = SceneManager.GetSceneAt(i);
+                if (loadedLevlel.name.Contains("Level "))
+                {
+                    SceneManager.SetActiveScene(loadedLevlel);
+                    loadedLevelBuildIndex = loadedLevlel.buildIndex;
+                    return;
+                }
+            }
+        }
+        StartCoroutine(LoadLevel(2));
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(creatKey))
-            CreatObject();
-
-        if (Input.GetKeyDown(beginKey))
+            CreatShape();
+        else if (Input.GetKeyDown(destoryKey))
+            DestoryShape();
+        else if (Input.GetKeyDown(beginKey))
             BeginNewGame();
-
-        if (Input.GetKeyDown(saveKey))
+        else if (Input.GetKeyDown(saveKey))
         {
-            storage.Save(this,saveVersion);
+            storage.Save(this, saveVersion);
         }
-
-        if (Input.GetKeyDown(loadKey))
+        else if (Input.GetKeyDown(loadKey))
         {
             BeginNewGame();
             storage.Load(this);
         }
+        else
+        {
+            for (int i = 1; i <= LevelCount; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Keypad0 + i))
+                {
+                    BeginNewGame();
+                    StartCoroutine(LoadLevel(i));
+                    return;
+                }
+            }
+        }
+
+        creationProgress += Time.deltaTime * CreationSpeed;
+        while (creationProgress >= 1f)
+        {
+            creationProgress -= 1f;
+            CreatShape();
+        }
+
+        destructionProgress += Time.deltaTime * DestructionSpeed;
+        while (destructionProgress >= 1f)
+        {
+            destructionProgress -= 1f;
+            DestoryShape();
+        }
     }
 
-    void CreatObject()
+    void CreatShape()
     {
         Shape instance = shapeFactory.GetRandom();
         Transform t = instance.transform;
@@ -61,29 +117,58 @@ public class Game : PersistableObject
         //改变颜色HSV
         //https://blog.csdn.net/zgjllf1011/article/details/79391241
         instance.SetColor(Random.ColorHSV(
-            hueMin : 0f,hueMax : 1f,
-            saturationMin : 0.5f,saturationMax : 1f,
-            valueMin : 0.25f,valueMax : 1f,
-            alphaMin : 1f,alphaMax : 1f
+            hueMin: 0f, hueMax: 1f,
+            saturationMin: 0.5f, saturationMax: 1f,
+            valueMin: 0.25f, valueMax: 1f,
+            alphaMin: 1f, alphaMax: 1f
         ));
 
         shapes.Add(instance);
     }
 
+    void DestoryShape()
+    {
+        if (shapes.Count > 0)
+        {
+            int index = Random.Range(0, shapes.Count);
+            shapeFactory.Reclaim(shapes[index]);
+            int lastIndex = shapes.Count - 1;
+            shapes[index] = shapes[lastIndex];
+            shapes.RemoveAt(lastIndex);
+        }
+    }
+
     void BeginNewGame()
     {
-        foreach (var obj in shapes)
+        for (var i = 0; i < shapes.Count; i++)
         {
-            Destroy(obj.gameObject);
+            shapeFactory.Reclaim(shapes[i]);
         }
 
 
         shapes.Clear();
     }
 
+    IEnumerator LoadLevel(int levelBuildIndex)
+    {
+        enabled = false;
+        if (loadedLevelBuildIndex > 0)
+        {
+            yield return SceneManager.UnloadSceneAsync(loadedLevelBuildIndex);
+        }
+        //异步加载场景
+        yield return SceneManager.LoadSceneAsync(
+               levelBuildIndex, LoadSceneMode.Additive
+            );
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(levelBuildIndex));
+        loadedLevelBuildIndex = levelBuildIndex;
+        enabled = true;
+    }
+
     public override void Save(GameDataWriter writer)
     {
         writer.Write(shapes.Count);
+        writer.Write(loadedLevelBuildIndex);
         for (int i = 0; i < shapes.Count; i++)
         {
             writer.Write(shapes[i].ShapeId);
@@ -102,6 +187,7 @@ public class Game : PersistableObject
         }
 
         int count = version <= 0 ? -version : reader.ReadInt();
+        StartCoroutine(LoadLevel(version < 2 ? 1 : reader.ReadInt()));
 
         for (int i = 0; i < count; i++)
         {
--- C#/MainTutorials/Assets/Object Management/持久化存储对象/Scripts/ShapeFactory.cs
+++ C#/MainTutorials/Assets/Object Management/持久化存储对象/Scripts/ShapeFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 //资源类，根据已设置好的资源来创建实例
@@ -9,21 +10,104 @@ public class ShapeFactory : ScriptableObject
 {
     [SerializeField]
     Shape[] prefabs;
-	[SerializeField]
-	Material[] materials;
-	
-    public Shape Get(int shapeId = 0,int materialId = 0)
+    [SerializeField]
+    Material[] materials;
+
+    [SerializeField]
+    private bool recycle;
+
+    //对象池
+    private List<Shape>[] pools;
+
+    //场景池
+    private Scene poolScene;
+
+    public Shape Get(int shapeId = 0, int materialId = 0)
     {
-        Shape instance = Instantiate(prefabs[shapeId]);
-		instance.ShapeId = shapeId;
-		instance.SetMaterial(materials[materialId],materialId);
-		return instance;
+        Shape instance;
+        if (recycle)
+        {
+            if (pools == null)
+                CreatPools();
+
+            List<Shape> pool = pools[shapeId];
+            int lastIndex = pool.Count - 1;
+            if (lastIndex >= 0)
+            {
+                instance = pool[lastIndex];
+                instance.gameObject.SetActive(true);
+                pool.RemoveAt(lastIndex);
+            }
+            else
+            {
+                instance = Instantiate(prefabs[shapeId]);
+                instance.ShapeId = shapeId;
+                //移动至指定场景中
+                SceneManager.MoveGameObjectToScene(instance.gameObject, poolScene);
+            }
+        }
+        else
+        {
+            instance = Instantiate(prefabs[shapeId]);
+            instance.ShapeId = shapeId;
+        }
+
+        instance.SetMaterial(materials[materialId], materialId);
+        return instance;
     }
 
     public Shape GetRandom()
     {
-		return Get(Random.Range(0,prefabs.Length),
-			Random.Range(0,materials.Length));
+        return Get(Random.Range(0, prefabs.Length),
+            Random.Range(0, materials.Length));
     }
+
+    //回收
+    public void Reclaim(Shape shapeToRecycle)
+    {
+        if (recycle)
+        {
+            if (pools == null)
+                CreatPools();
+            pools[shapeToRecycle.ShapeId].Add(shapeToRecycle);
+            shapeToRecycle.gameObject.SetActive(false);
+        }
+        else
+        {
+            Destroy(shapeToRecycle.gameObject);
+        }
+    }
+
+    //初始化对象池
+    void CreatPools()
+    {
+        pools = new List<Shape>[prefabs.Length];
+        //自动编译之后，序列化数据会丢失
+        if (Application.isEditor)
+        {
+            poolScene = SceneManager.GetSceneByName("ShapeFactory");
+            if (poolScene.isLoaded)
+            {
+                GameObject[] rootObjects = poolScene.GetRootGameObjects();
+                for (int i = 0; i < rootObjects.Length; i++)
+                {
+                    Shape pooledShape = rootObjects[i].GetComponent<Shape>();
+                    if (!pooledShape.gameObject.activeSelf)
+                    {
+                        pools[pooledShape.ShapeId].Add(pooledShape);
+                    }
+                }
+                return;
+            }
+        }
+        poolScene = SceneManager.CreateScene("ShapeFactory");
+
+        for (int i = 0; i < pools.Length; i++)
+        {
+            pools[i] = new List<Shape>();
+        }
+    }
+
+
 
 }
