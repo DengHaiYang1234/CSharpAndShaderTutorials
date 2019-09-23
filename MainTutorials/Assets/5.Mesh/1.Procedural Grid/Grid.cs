d48c4eeff59e6f70aee76ca5b3520f35e174a497
429078038@qq.com <429078038@qq.com>
2019-09-23 22:16:33 +0800

    2019.9.23
    提交Mesh第一部分

--- MainTutorials/Assets/5.Mesh/1.Procedural Grid/Grid.cs
+++ MainTutorials/Assets/5.Mesh/1.Procedural Grid/Grid.cs
@@ -3,53 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
-public class Grid : MonoBehaviour
+namespace Tests
 {
-    public int xSize, ySize;
-
-    private Vector3[] vertices;
-
-    private Mesh mesh;
-
-    private void Awake()
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class Grid : MonoBehaviour
     {
-        StartCoroutine(Generate());
-    }
+        public int xSize, ySize;
 
-    IEnumerator Generate()
-    {
-        WaitForSeconds wait = new WaitForSeconds(0.05f);
+        private Vector3[] vertices;
 
-        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
-        mesh.name = "Procedural Grid";
-        vertices = new Vector3[(xSize + 1)*(ySize + 1)];
-        for (int i = 0, y = 0; y <= ySize; y++)
+        private Mesh mesh;
+
+        private void Awake()
         {
-            for (int x = 0; x <= xSize; x++ ,i++)
+            Generate();
+        }
+
+        void Generate()
+        {
+            GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+            mesh.name = "Procedural Grid";
+            vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+            Vector4[] tangents = new Vector4[vertices.Length];
+            Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+            Vector2[] uv = new Vector2[vertices.Length];
+            for (int i = 0, y = 0; y <= ySize; y++)
             {
-                vertices[i] = new Vector3(x, y);
-                yield return wait;
+                for (int x = 0; x <= xSize; x++, i++)
+                {
+                    vertices[i] = new Vector3(x, y);
+                    uv[i] = new Vector2((float)x/xSize, (float)y/ySize);
+                    tangents[i] = tangent;
+                }
+            }
+            mesh.vertices = vertices;
+            mesh.uv = uv;
+            mesh.tangents = tangents;
+            //顺时针
+            int[] triangles = new int[xSize * ySize * 6];//总共的三角形个数
+                                                         //ti:6个点构成两个三角形。最终形成一个平行四边形
+            for (int ti = 0, vi = 0, y = 0; y < ySize ; y++, vi++)
+            {
+                for (int x = 0; x < xSize; x++, ti += 6, vi++)
+                {
+                    triangles[ti] = vi;//左下点
+                    triangles[ti + 3] = triangles[ti + 2] = vi + 1;//右下点
+                    triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;//左上点
+                    triangles[ti + 5] = vi + xSize + 2;//右上点
+                }
+            }
+            mesh.triangles = triangles;
+            mesh.RecalculateNormals();
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (vertices == null)
+                return;
+            Gizmos.color = Color.black;
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Gizmos.DrawSphere(vertices[i], 0.1f);
             }
         }
-        mesh.vertices = vertices;
 
-        int[] triangles = new int[3];
-        triangles[0] = 0;
-        triangles[1] = xSize + 1;
-        triangles[2] = 1;
-        mesh.triangles = triangles;
     }
-        
-    private void OnDrawGizmos()
-    {
-        if (vertices == null)
-            return;
-        Gizmos.color = Color.black;
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            Gizmos.DrawSphere(vertices[i], 0.1f);
-        }
-    }
-
 }
+
