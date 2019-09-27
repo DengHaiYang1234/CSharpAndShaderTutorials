9110915a0ca04dd7a394a4dd3cb156f379ba2c45
denghaiyang <429078038@qq.com>
2019-09-27 17:31:47 +0800

    2019.9.27
    1.完成CubeMesh的制作
    2.添加Render中文翻译教程的地址

--- MainTutorials/Assets/5.Mesh/2.Rounded Cube/RoundedCube.cs
+++ MainTutorials/Assets/5.Mesh/2.Rounded Cube/RoundedCube.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-public class Cube : MonoBehaviour
+public class RoundedCube : MonoBehaviour
 {
 
-    public int xSize, ySize, zSize;
+    public int xSize, ySize, zSize, roundness;
     private Mesh mesh;
     private Vector3[] vertices;
+    //法线
+    Vector3[] normals;
+    Color32[] cubeUV;
 
     /// <summary>
     /// 构建三角形
@@ -41,6 +45,7 @@ public class Cube : MonoBehaviour
 
         CreateVertices();
         CreateTriangles();
+        CreateColliders();
     }
 
     private void CreateVertices()
@@ -56,6 +61,8 @@ public class Cube : MonoBehaviour
             (xSize - 1) * (zSize - 1) +
             (ySize - 1) * (zSize - 1)) * 2;
         vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+        normals = new Vector3[vertices.Length];
+        cubeUV = new Color32[vertices.Length];
 
         int v = 0;
         //y：高度
@@ -64,22 +71,22 @@ public class Cube : MonoBehaviour
             //x边
             for (int x = 0; x <= xSize; x++)
             {
-                vertices[v++] = new Vector3(x, y, 0);
+                SetVertex(v++, x, y, 0);
             }
             //z边
             for (int z = 1; z <= zSize; z++)
             {
-                vertices[v++] = new Vector3(xSize, y, z);
+                SetVertex(v++, xSize, y, z);
             }
             //与上面x边平行的另一边
             for (int x = xSize - 1; x >= 0; x--)
             {
-                vertices[v++] = new Vector3(x, y, zSize);
+                SetVertex(v++, x, y, zSize);
             }
             //与上面z边平行的另一边
             for (int z = zSize - 1; z > 0; z--)
             {
-                vertices[v++] = new Vector3(0, y, z);
+                SetVertex(v++, 0, y, z);
             }
         }
 
@@ -88,7 +95,7 @@ public class Cube : MonoBehaviour
         {
             for (int x = 1; x < xSize; x++)
             {
-                vertices[v++] = new Vector3(x, ySize, z);
+                SetVertex(v++, x, ySize, z);
             }
         }
 
@@ -97,11 +104,13 @@ public class Cube : MonoBehaviour
         {
             for (int x = 1; x < xSize; x++)
             {
-                vertices[v++] = new Vector3(x, 0, z);
+                SetVertex(v++, x, 0, z);
             }
         }
 
         mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.colors32 = cubeUV;
     }
     //四个面的三角    
     private void CreateTriangles()
@@ -110,26 +119,47 @@ public class Cube : MonoBehaviour
         int quads = (xSize * ySize + xSize * zSize + ySize * zSize) * 2;
         //一个四边形是由6个顶点构成（不在乎是否有重合的顶点）
         int[] triangles = new int[quads * 6];
+
+        int[] trianglesZ = new int[(xSize * ySize) * 12];//12 : 2 * 6  两面*三角形数量
+        int[] trianglesX = new int[(ySize * zSize) * 12];
+        int[] trianglesY = new int[(xSize * zSize) * 12];
+
         //完整的在y层四条边的顶点数
         int ring = (xSize + zSize) * 2;
         //t：起始顶点索引（6个一轮回）  v：起始递增索引
-        int t = 0, v = 0;
+        int tZ = 0, tX = 0, tY = 0, t = 0, v = 0;
 
         for (int y = 0; y < ySize; y++, v++)
         {
-            for (int q = 0; q < ring - 1; q++, v++)
+            for (int q = 0; q < xSize; q++, v++)
             {
-                t = SetQuad(triangles, t, v, v + 1, v + ring, v + ring + 1);
+                tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
             }
 
+            for (int q = 0; q < zSize; q++, v++)
+            {
+                tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
+            }
+            for (int q = 0; q < xSize; q++, v++)
+            {
+                tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
+            }
+            for (int q = 0; q < zSize - 1; q++, v++)
+            {
+                tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
+            }
             //最后一个点的右下和右上需要回到初始点
-            t = SetQuad(triangles, t, v, v - ring + 1, v + ring, v + 1);
+            tX = SetQuad(trianglesX, tX, v, v - ring + 1, v + ring, v + 1);
         }
 
-        t = CreatTopFace(triangles, t, ring);
-        t = CreatBottomFace(triangles, t, ring);
+        tY = CreatTopFace(trianglesY, tY, ring);
+        tY = CreatBottomFace(trianglesY, tY, ring);
 
-        mesh.triangles = triangles;
+        mesh.subMeshCount = 3;
+        mesh.SetTriangles(trianglesZ,0);
+        mesh.SetTriangles(trianglesX,1);
+        mesh.SetTriangles(trianglesY,2);
+        //mesh.triangles = triangles;
     }
 
 
@@ -174,7 +204,7 @@ public class Cube : MonoBehaviour
         int vTop = vMin - 2;
         //第四排第一个
         t = SetQuad(triangles, t, vMin, vMid, vTop + 1, vTop);
-        for (int x = 0; x < xSize - 1; x++, vTop--, vMid++)
+        for (int x = 1; x < xSize - 1; x++, vTop--, vMid++)
         {
             //第四排第二个
             t = SetQuad(triangles, t, vMid, vMid + 1, vTop, vTop - 1);
@@ -226,8 +256,85 @@ public class Cube : MonoBehaviour
             t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vMid + 1);
         }
         t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vTop - 2);
-
         return t;
+    }
+
+    void CreateColliders()
+    {
+        AddBoxCollider(xSize,ySize - roundness * 2,zSize - roundness * 2);
+        AddBoxCollider(xSize - roundness * 2,ySize,zSize - roundness * 2);
+        AddBoxCollider(xSize - roundness * 2,ySize - roundness * 2,zSize);
+
+        Vector3 min = Vector3.one * roundness;
+		Vector3 half = new Vector3(xSize, ySize, zSize) * 0.5f; 
+		Vector3 max = new Vector3(xSize, ySize, zSize) - min;
+
+		AddCapsuleCollider(0, half.x, min.y, min.z);
+		AddCapsuleCollider(0, half.x, min.y, max.z);
+		AddCapsuleCollider(0, half.x, max.y, min.z);
+		AddCapsuleCollider(0, half.x, max.y, max.z);
+		
+		AddCapsuleCollider(1, min.x, half.y, min.z);
+		AddCapsuleCollider(1, min.x, half.y, max.z);
+		AddCapsuleCollider(1, max.x, half.y, min.z);
+		AddCapsuleCollider(1, max.x, half.y, max.z);
+		
+		AddCapsuleCollider(2, min.x, min.y, half.z);
+		AddCapsuleCollider(2, min.x, max.y, half.z);
+		AddCapsuleCollider(2, max.x, min.y, half.z);
+		AddCapsuleCollider(2, max.x, max.y, half.z);
+    }
+
+    void AddBoxCollider(float x,float y,float z)
+    {
+        BoxCollider c = gameObject.AddComponent<BoxCollider>();
+        c.size = new Vector3(x,y,z);
+    }
+
+    private void AddCapsuleCollider (int direction, float x, float y, float z) {
+		CapsuleCollider c = gameObject.AddComponent<CapsuleCollider>();
+		c.center = new Vector3(x, y, z);
+		c.direction = direction;
+		c.radius = roundness;
+		c.height = c.center[direction] * 2f;
+	}
+
+    void SetVertex(int i, int x, int y, int z)
+    {
+        Vector3 inner = vertices[i] = new Vector3(x, y, z);
+
+
+        //法线计算
+        if (x < roundness)
+        {
+            inner.x = roundness;
+        }
+        else if (x > xSize - roundness)
+        {
+            inner.x = xSize - roundness;
+        }
+
+        if (y < roundness)
+        {
+            inner.y = roundness;
+        }
+        else if (y > ySize - roundness)
+        {
+            inner.y = ySize - roundness;
+        }
+
+        if (z < roundness)
+        {
+            inner.z = roundness;
+        }
+        else if (z > zSize - roundness)
+        {
+            inner.z = zSize - roundness;
+        }
+
+        normals[i] = (vertices[i] - inner).normalized;
+        vertices[i] = inner + normals[i] * roundness;
+        cubeUV[i] = new Color32((byte)x,(byte)y,(byte)z,0);
     }
 
 
@@ -238,10 +345,12 @@ public class Cube : MonoBehaviour
         {
             return;
         }
-        Gizmos.color = Color.black;
         for (int i = 0; i < vertices.Length; i++)
         {
+            Gizmos.color = Color.black;
             Gizmos.DrawSphere(vertices[i], 0.1f);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(vertices[i], normals[i]);
         }
     }
 }
