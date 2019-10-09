f1ac7ff753af93309b811c52a970d6a460adab2b
denghaiyang <429078038@qq.com>
2019-10-09 19:10:55 +0800

    2019.10.9
    矩阵部分

---
+++ MainTutorials/Assets/6.Render/1.矩阵/Transformation.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Transformation : MonoBehaviour 
+{
+	public abstract Vector3 Apply(Vector3 point);
+
+}
