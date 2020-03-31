07d8030e6505d3a8698f85dd24d2330ed570a26a
429078038@qq.com <429078038@qq.com>
2020-03-31 20:49:29 +0800

    2020.3.31
    新的征程已经开始！
    贝塞尔曲线
    头有点痛，明天弄

---
+++ MainTutorials/Assets/8.Beziér/Bezier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Bezier
+{
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+    }
+}
---
+++ MainTutorials/Assets/8.Beziér/Editor/BezierCurveInspector.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BezierCurve))]
+public class BezierCurveInspector : Editor
+{
+    private BezierCurve curve;
+    private Transform handleTransform;
+    private Quaternion handleRotation;
+    private const int lineSteps = 10;
+
+    private void OnSceneGUI()
+    {
+        curve = target as BezierCurve;
+        handleTransform = curve.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+            handleTransform.rotation : Quaternion.identity;
+
+        Vector3 p0 = ShowPoint(0);
+        Vector3 p1 = ShowPoint(1);
+        Vector3 p2 = ShowPoint(2);
+
+        Handles.color = Color.gray;
+        Handles.DrawLine(p0, p1);
+        Handles.DrawLine(p1, p2);
+
+        Handles.color = Color.white;
+        Vector3 lineStart = curve.GetPoint(0f);
+        for (int i = 0; i <= lineSteps; i++)
+        {
+            Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(lineStart,lineEnd);
+            lineStart = lineEnd;
+        }
+    }
+
+    private Vector3 ShowPoint(int index)
+    {
+        Vector3 point = handleTransform.TransformPoint(curve.points[index]);
+        EditorGUI.BeginChangeCheck();
+        point = Handles.DoPositionHandle(point, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(curve, "Move Point");
+            EditorUtility.SetDirty(curve);
+            curve.points[index] = handleTransform.InverseTransformPoint(point);
+        }
+        return point;
+    }
+}
