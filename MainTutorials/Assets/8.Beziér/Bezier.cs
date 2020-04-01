d208c5ce6f42d2b92de616813fdc3e69ef79fa71
429078038@qq.com <429078038@qq.com>
2020-04-01 21:43:10 +0800

    20120.4.1
    继续贝塞尔曲线

--- MainTutorials/Assets/8.Beziér/Bezier.cs
+++ MainTutorials/Assets/8.Beziér/Bezier.cs
@@ -4,8 +4,41 @@ using UnityEngine;
 
 public static class Bezier
 {
+    //二阶
     public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
     {
-        return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+        //Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t); 由这个推导而来
+        //B(t) = (1 - t) P0 + t P1.
+        //B(t) = (1 - t) ((1 - t) P0 + t P1) + t ((1 - t) P1 + t P2).
+        //B(t) = (1 - t) * (1 - t) * P0 + 2 * (1 - t) * t * P1 + t * t * P2.
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+
+        return oneMinusT * oneMinusT * p0 + 2f * oneMinusT * t * p1 + t * t * p2;
+    }
+
+    //二阶切线
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        return 2f * (1f - t) * (p1 - p0) + 2f * t * (p2 - p1);
+    }
+
+    //三阶
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        //B(t) = (1 - t)3 P0 + 3 (1 - t)2 t P1 + 3 (1 - t) t2 P2 + t3 P3
+
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+
+        return oneMinusT * oneMinusT * oneMinusT * p0 + 3f * oneMinusT * oneMinusT * t * p1 + 3f * oneMinusT * t * t * p2 + t * t * t * p3;
+    }
+
+    //三阶切线
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return 3f * oneMinusT * oneMinusT * (p1 - p0) + 6f * oneMinusT * t * (p2 - p1) + 3f * t * t * (p3 - p2);
     }
 }
--- MainTutorials/Assets/8.Beziér/Editor/BezierCurveInspector.cs
+++ MainTutorials/Assets/8.Beziér/Editor/BezierCurveInspector.cs
@@ -11,6 +11,8 @@ public class BezierCurveInspector : Editor
     private Quaternion handleRotation;
     private const int lineSteps = 10;
 
+    private float directionScale = 0.5f;
+
     private void OnSceneGUI()
     {
         curve = target as BezierCurve;
@@ -21,18 +23,25 @@ public class BezierCurveInspector : Editor
         Vector3 p0 = ShowPoint(0);
         Vector3 p1 = ShowPoint(1);
         Vector3 p2 = ShowPoint(2);
+        Vector3 p3 = ShowPoint(3);
 
         Handles.color = Color.gray;
         Handles.DrawLine(p0, p1);
-        Handles.DrawLine(p1, p2);
+        Handles.DrawLine(p2, p3);
 
-        Handles.color = Color.white;
-        Vector3 lineStart = curve.GetPoint(0f);
+        ShowDirections();
+        Handles.DrawBezier(p0, p3, p1, p2,Color.white,null,2f);
+    }
+
+    void ShowDirections()
+    {
+        Handles.color = Color.green;
+        Vector3 point = curve.GetPoint(0f);
+        Handles.DrawLine(point, point + curve.GetDirection(0f) * directionScale);
         for (int i = 0; i <= lineSteps; i++)
         {
-            Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
-            Handles.DrawLine(lineStart,lineEnd);
-            lineStart = lineEnd;
+            point = curve.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(point,point + curve.GetDirection(i / (float)lineSteps) * directionScale);
         }
     }
 
